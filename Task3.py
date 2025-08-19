2eb461f9cc3c2f3287b83d8fcab425452badfb89
Akinpelu Oluwatosin <fortifiedtosin@gmail.com>
2025-08-19 16:08:54 +0100

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,26 @@
+def get_likes_message(names):
+
+    if not names:
+
+        return "no one likes this"
+
+    elif len(names) == 1:
+
+        return f"{names[0]} likes this"
+
+    elif len(names) == 2:
+
+        return f"{names[0]} and {names[1]} like this"
+
+    elif len(names) == 3:
+
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+
+    else:
+
+        return f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
+
+
+names_list = ["Peter", "Alex", "Jacob"]
+
+print(get_likes_message(names_list))
